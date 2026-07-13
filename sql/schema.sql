fdac489f0cce4d91b24a5e3f69ec6442d7b89686
Heitor <161908829+Heitor-M33n@users.noreply.github.com>
2026-07-13 18:20:23 -0300

    Rename tables to lowercase in schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,16 +1,16 @@
-CREATE TABLE Produtos (
+CREATE TABLE produtos (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     estoque INT NOT NULL,
     preco NUMERIC(10,2) NOT NULL
 );
 
-CREATE TABLE Vendas (
+CREATE TABLE vendas (
     id SERIAL PRIMARY KEY,
     data TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE TABLE ItensVenda (
+CREATE TABLE itens_venda (
     id_venda INT NOT NULL,
     id_produto INT NOT NULL,
     quantidade INT NOT NULL,
